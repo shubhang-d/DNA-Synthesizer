3d4b551d8678e61c4bcd7bba1d34997e04231f0d
Shubhang Dixit <shubhangd205@gmail.com>
2026-05-02 00:03:10 +0530

    redeploy

--- app/layout.jsx
+++ app/layout.jsx
@@ -8,7 +8,7 @@ const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
-
+//creating a redeploy
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
